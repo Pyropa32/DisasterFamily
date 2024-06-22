6f172a97ee36da8d7379f818d59e153607243443
dygo454 <diegoaguilar454@yahoo.com>
2024-06-22 12:55:23 +0900

    added UI function for just pure vector conversion

--- Assets/Diego-UI-Package/UIScripts/InteractGame.cs
+++ Assets/Diego-UI-Package/UIScripts/InteractGame.cs
@@ -13,4 +13,12 @@ public class InteractGame {
         RaycastHit2D hit = Physics2D.GetRayIntersection(new Ray(GameObject.FindWithTag("MainCamera").transform.position + gameSpace, GameObject.FindWithTag("MainCamera").transform.forward));
         return hit.transform;
     }
+    public static Vector2 GetGameSpaceFromScreenSpace(Vector2 pos) {
+        Vector2 gameSpace = new Vector2(pos.x / Screen.width - 0.5f, pos.y / Screen.height - 0.5f);
+        gameSpace.x *= 16;
+        gameSpace.y *= 10;
+        gameSpace += new Vector2(1.2f, 0.75f);
+        gameSpace.x *= 16 / 13.6f;
+        gameSpace.y *= 10 / 8.5f;
+    }
 }
