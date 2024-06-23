0487e539c76561ad2f959b1cf4096762306dedaf
dylan <dylan.zhao@ufl.edu>
2024-06-23 12:28:34 +0900

    Item Lookup

--- Assets/Scripts/MonoBehaviour/ClickToMove.cs
+++ Assets/Scripts/MonoBehaviour/ClickToMove.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ClickToMove: MonoBehaviour
+using Prototypal;
+public class ClickToMove : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
     private StoryCommandDispatcher dispatcher;
-    private Actor myActor;
+    private SimpleActor myActor;
 
     private bool _previousLeftMouseDown = false;
     void Start()
     {
-        myActor = GetComponent<Actor>();
+        myActor = GetComponent<SimpleActor>();
     }
 
     // Update is called once per frame
@@ -21,27 +21,93 @@ public class ClickToMove: MonoBehaviour
         var currentLeftMouseDown = Input.GetMouseButtonDown(0);
         if (currentLeftMouseDown == false && _previousLeftMouseDown == true)
         {
-            var worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //OnClicked(worldMousePosition);
-            
+            Vector2 worldMousePosition = InteractGame.GetGameSpaceFromScreenSpace(Input.mousePosition);
+            Vector3 CameraPos = GameObject.FindWithTag("MainCamera").transform.position;
+            worldMousePosition += new Vector2(CameraPos.x, CameraPos.y);
+            OnClicked(worldMousePosition);
+
         }
         _previousLeftMouseDown = currentLeftMouseDown;
     }
 
-    private void SendToDispatcher()
-    {
-
-    }
-
-   /* private void OnClicked(Vector2 where)
+    private void OnClicked(Vector2 where)
     {
         // Get the coordinates expressed in plane coords.
-        var planeCoordinates = myActor.CurrentPlane.ScreenToPlane(where); 
-        // Create list of move commands using the Graph, with A* pathfinding.
-        // FIXME: For now, since no Pathfinding exists, just create one Move command.
-        var moveCommand = new MoveActorStoryCommand(myActor, myActor.LocalPosition, planeCoordinates, myActor.MovementSpeed);
-        // Send the move commands to the dispatcher.
-        dispatcher.Receive(moveCommand);
 
-    }*/
+        // see if you clicked on another plane.
+        var otherPlane = myActor.World.GetPlaneByPosition(where);
+
+        if (otherPlane != myActor.CurrentPlane && otherPlane != null)
+        {
+            // get the location of the click for otherPlane.
+            var clickedCoordinates = otherPlane.ScreenToPlane(where);
+            // get the list of planes to navigate when getting to the other plane.
+            var externalPath = myActor.World.GetShortestExternalPath(myActor.CurrentPlane, otherPlane);
+
+            // pathfinding failure
+            if (!externalPath.Success)
+            {
+                Debug.Log("Failed to find path between " + myActor.CurrentPlane.name + " and " + otherPlane);
+                return;
+            }
+
+            List<IStoryCommand> movementCommandChain = new List<IStoryCommand>();
+
+            var lastPosition = myActor.LocalPosition;
+
+            for (int i = 0; i < externalPath.Solution.Length; i++)
+            {
+                var currentPlanePair = externalPath.Solution[i];
+                var firstPlane = currentPlanePair.StartPlane;
+                var gateway = currentPlanePair.Gate;
+                var secondPlane = currentPlanePair.DestinationPlane;
+                // move towards gateway
+                // on finish, set plane to secondPlane;
+                Vector2 startPosition = Vector2.zero;
+                if (i == 0)
+                {
+                    startPosition = myActor.LocalPosition;
+                }
+                else
+                {
+                    startPosition = lastPosition;
+                }
+                var destination = firstPlane.ScreenToPlane(gateway.transform.position);
+
+                // this argument (adjacent) is what causes the gateway change.
+                var moveCommand = new MoveActorStoryCommand(
+                    myActor,
+                    startPosition,
+                    destination,
+                    myActor.MovementSpeed,
+                    _adjacent: secondPlane
+                );
+
+                movementCommandChain.Add(moveCommand);
+
+                lastPosition = secondPlane.ScreenToPlane(gateway.transform.position);
+            }
+            // The last movement command in this series is just going to be a basic move command
+            var internalMoveCommand = new MoveActorStoryCommand(myActor,
+                                                                lastPosition,
+                                                                otherPlane.ScreenToPlane(where),
+                                                                myActor.MovementSpeed
+                                                                );
+            movementCommandChain.Add(internalMoveCommand);
+            // Send all of the commands to the dispatcher.
+            dispatcher.ReceiveSequentialRange(
+                movementCommandChain,
+                blocking:false,
+                flagOverride:StoryCommandExecutionFlags.DiscardAlike
+                );
+        }
+        else
+        {
+            // move around in local space
+            var planeCoordinates = myActor.CurrentPlane.ScreenToPlane(where);
+            var moveCommand = new MoveActorStoryCommand(myActor, myActor.LocalPosition, planeCoordinates, myActor.MovementSpeed);
+            dispatcher.Receive(moveCommand);
+        }
+
+    }
 }
--- Assets/Scripts/Other/MoveActorStoryCommand.cs
+++ Assets/Scripts/Other/MoveActorStoryCommand.cs
@@ -1,26 +1,37 @@
-﻿/*using System;
+﻿using System;
 using UnityEngine;
-
+using Prototypal;
 public sealed class MoveActorStoryCommand : IStoryCommand
 {
 
-    public event Func<object> OnFinish;
+    // object = args
+    public event Action<object> OnFinish;
     public Vector2 Destination { get; private set; }
     public Vector2 CurrentPosition => Vector2.Lerp(startPosition, Destination, progress);
     public float MovementSpeed { get; private set; }
     public bool IsFinished { get => finished; }
     public bool IsConcurrent { get => true; }
-    public bool IsStarted { get => true; }
+    public bool IsStarted { get => started; }
     public StoryCommandExecutionFlags ExecutionFlags => StoryCommandExecutionFlags.DiscardAlike |
                                                         StoryCommandExecutionFlags.DiscardConcurrent;
-    private Actor actor;
+    private SimpleActor actor;
     private Vector2 startPosition;
     private float progress = 0f;
     private bool finished = false;
     private bool started = false;
+    private SimpleFloorPlane adjacent;
 
-    public MoveActorStoryCommand(Actor _actor, Vector2 _start, Vector2 _end, float _movementSpeed)
+    public MoveActorStoryCommand(SimpleActor _actor, Vector2 _start, Vector2 _end, float _movementSpeed, SimpleFloorPlane _adjacent=null)
     {
+        if (_adjacent != null)
+        {
+            adjacent = _adjacent;
+            OnFinish = (object arg) =>
+            {
+                _actor.DoSetCurrentPlane((SimpleFloorPlane)arg);
+            };
+        }
+        
         startPosition = _start;
         Destination = _end;
 
@@ -29,12 +40,27 @@ public sealed class MoveActorStoryCommand : IStoryCommand
         var dist = Vector2.Distance(plane.PlaneToScreen(_start), plane.PlaneToScreen(_end));
         _movementSpeed /= dist;
 
-        // FIXME: There's one more thing
-        // You move slightly faster if you walk at an angle perpendicular to the angle ➚ from BottomLeft to TopRight ➚
-        var diagonal = plane.TopRight - plane.BottomLeft;
-        // 1.0 = walking perpendicular. 0.0 = walking parallel to diagonal.
-        var perpendicularity = 1.0 - Mathf.Abs(Vector2.Dot(diagonal.normalized, (_end - _start).normalized));
-        // Use perpendicularity to negate the extra walk speed.
+        var bottomToTopDiagonal = plane.TopRight - plane.BottomLeft;
+        var topToBottomDiagonal = plane.TopLeft - plane.BottomRight;
+        var ratio = bottomToTopDiagonal.sqrMagnitude / topToBottomDiagonal.sqrMagnitude;
+        var direction = (_end - _start).normalized;
+        // ratio is 1-1 in normal situations, so division shouldn't hurt.
+        // both are square(d), so who cares(d)
+
+        // divide by the ratio when perpendicularity is lowest.
+        // AKA, when dot product is 0.
+        var perpendicularity = Vector2.Dot(bottomToTopDiagonal.normalized, direction.normalized);
+
+        // divide movement speed fully if moving fully perpendicularly.
+        _movementSpeed /= Mathf.Lerp(ratio, 1f, Math.Abs(perpendicularity));
+
+
+        // // FIXME: There's one more thing
+        // // You move slightly faster if you walk at an angle perpendicular to the angle ➚ from BottomLeft to TopRight ➚
+        // var diagonal = plane.TopRight - plane.BottomLeft;
+        // // 1.0 = walking perpendicular. 0.0 = walking parallel to diagonal.
+        // var perpendicularity = 1.0 - Mathf.Abs(Vector2.Dot(diagonal.normalized, (_end - _start).normalized));
+        // // Use perpendicularity to negate the extra walk speed.
 
         MovementSpeed = _movementSpeed;
         actor = _actor;
@@ -51,6 +77,7 @@ public sealed class MoveActorStoryCommand : IStoryCommand
         actor.LocalPosition = startPosition;
         // mayhaps?
         actor.SetAnim("walk");
+        Debug.Log("started walk command");
     }
 
     public void Tick(float delta)
@@ -62,7 +89,7 @@ public sealed class MoveActorStoryCommand : IStoryCommand
             {
                 finished = true;
                 actor.SetAnim("idle");
-                OnFinish?.Invoke();
+                OnFinish?.Invoke(adjacent);
             }
             else
             {
@@ -71,4 +98,3 @@ public sealed class MoveActorStoryCommand : IStoryCommand
         }
     }
 }
-*/
