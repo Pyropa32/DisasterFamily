3a4e1c2617efae7fffe21593c5ae982fc93e080c
Dygo454 <56135738+Dygo454@users.noreply.github.com>
2024-06-25 21:52:51 -0400

    ALYSSA'S most recent commit :)

--- Assets/Diego-UI-Package/alyssa-dialogue/DialogueScripts/DialogueManager.cs
+++ Assets/Diego-UI-Package/alyssa-dialogue/DialogueScripts/DialogueManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Xml;
 using UnityEngine;
 
+
 // Next up:
 // Include Actor data
 // Actor Expression Data as well
 
 public class DialogueManager {
+    // load dialogue from XML into dict
     private static Dictionary<string, string[]> DialogueDict = new Dictionary<string, string[]>();
 
     public static void loadFromFile(string path) {
@@ -18,15 +20,49 @@ public class DialogueManager {
         TextAsset textAsset = (TextAsset)Resources.Load(path);
         XmlDocument xmldoc = new XmlDocument();
         xmldoc.LoadXml(textAsset.text);
-        XmlNodeList pages = xmldoc.GetElementsByTagName("page");
+        XmlNodeList dialogue = xmldoc.GetElementsByTagName("dialogue");
 
-        string[] value = new string[pages.Count];
-        for (int i = 0; i < pages.Count; i++)
+        //should only be 1 dialogue tag in XML
+        if (dialogue.Count != 1)
         {
-            value[i] = pages[i].InnerText;
+            Debug.LogError("There should only be 1 dialogue tag! Check " + path);
+            return;
+        }
+        // follows XML format : dialogue > character name > interaction id > page
+        XmlNodeList characters = dialogue[0].ChildNodes;
+        for (int i = 0; i < characters.Count; i++)
+        {
+            XmlNodeList interactions = characters[i].ChildNodes;
+            for (int j = 0; j < interactions.Count; j++)
+            {
+                //changes dict key to be specific per character interaction
+                string key = path;
+                if (characters[i].Attributes.Count != 1 || characters[i].Attributes[0].Name != "name")
+                {
+                    Debug.LogError("We only want one attribute tag per character -- the name!");
+                    continue;
+                }
+
+                key += "." + characters[i].Attributes[0].Value;
+
+                if (interactions[j].Attributes.Count != 1 || interactions[j].Attributes[0].Name != "id")
+                {
+                    Debug.LogError("We only want one attribute tag per interactions -- the id!");
+                    continue;
+                }
+
+                key += "." + interactions[j].Attributes[0].Value;
+
+                XmlNodeList pages = interactions[j].ChildNodes;
+                string[] sequence = new string[pages.Count];
+                for (int k = 0; k < pages.Count; k++)
+                {
+                    sequence[k] = pages[k].InnerText;
+                }
+                DialogueDict.Add(key, sequence);
+            }
         }
 
-        DialogueDict.Add(path, value);
     }
 
     public static bool textToLoad(string path) {
@@ -39,3 +75,6 @@ public class DialogueManager {
     }
 
 }
+
+// to use : call textToLoad("FileName.characterName.interactionID")
+// need to know which character & which interaction to use >> see XML
