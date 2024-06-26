e1f1652c4342c188a3faba7ad56b8f80043a4608
Pyropa32 <pyropadev@gmail.com>
2024-06-26 20:57:30 +0900

    what even is this?

--- Assets/Scripts/MonoBehaviour/ItemMentionsListView.cs
+++ Assets/Scripts/MonoBehaviour/ItemMentionsListView.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Diego;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
