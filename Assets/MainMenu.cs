b009ba85e6152d260dd513c13504726d743ffea8
Dygo454 <diegoaguilar454@yahoo.com>
2024-07-24 12:18:48 +0900

    quick aspect ratio fix

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-   public void PlayGame()
-    {
+public class MainMenu : MonoBehaviour {
+    public void PlayGame() {
         SceneManager.LoadSceneAsync(1);
     }
-    
+
+    public void ToMainMenu() {
+        SceneManager.LoadSceneAsync(0);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
