62520d9f8a5f3dfaa2914a024817be26647f1183
Pyropa32 <pyropadev@gmail.com>
2024-07-24 16:37:10 +0900

    auto move on hold down

--- Assets/NuCode/PlayerAdapter.cs
+++ Assets/NuCode/PlayerAdapter.cs
@@ -10,6 +10,13 @@ using Diego;
 [RequireComponent(typeof(Character))]
 public class PlayerAdapter : MonoBehaviour
 {
+    // continuous click
+    // continuous
+    private const uint FRAMES_REQUIRED_FOR_HOLD_CLICK = 8;
+    private const uint FRAMES_FOR_AUTO_MOVE_FREQUENCY = 5;
+    private uint consecutiveHeldFrames = 0;
+    private bool IsMouseButtonHeld => consecutiveHeldFrames >= FRAMES_REQUIRED_FOR_HOLD_CLICK;
+
     bool previousLeftMouseDown = false;
     // Start is called before the first frame update
     private Character player;
@@ -21,10 +28,22 @@ public class PlayerAdapter : MonoBehaviour
         DialogueManager.NoOp();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Consistency!
+    void FixedUpdate()
     {
-        var currentLeftMouseDown = Input.GetMouseButtonDown(0);
+        var currentLeftMouseDownNow = Input.GetMouseButtonDown(0);
+        var currentLeftMouseDown = Input.GetMouseButton(0);
+        // update consecutiveness
+        if (currentLeftMouseDown)
+        {
+            consecutiveHeldFrames += 1;
+        }
+        else
+        {
+            consecutiveHeldFrames = 0;
+        }
+
+
         Vector2 worldMousePosition = CameraToScreenspaceConverter.GetGameSpaceFromScreenSpace(Input.mousePosition);
         bool inRange = Mathf.Abs(worldMousePosition.x) <= Camera.main.orthographicSize * 8 / 5;
         inRange = inRange && Mathf.Abs(worldMousePosition.y) <= Camera.main.orthographicSize;
@@ -32,38 +51,57 @@ public class PlayerAdapter : MonoBehaviour
         Camera UICamera = GameObject.FindWithTag("UIInclude").GetComponent<Camera>();
         Collider2D hit = Physics2D.Raycast(UICamera.ScreenToWorldPoint(Input.mousePosition), UICamera.transform.forward).collider;
         interacting = interacting || (hit?.GetComponent<DialogueTextManager>() != null && hit.transform.parent.GetComponent<SpriteRenderer>().enabled);
-        if (currentLeftMouseDown == false && previousLeftMouseDown == true && inRange == true && interacting == false && Timer.isPaused() == false) {
+
+        bool justClicked = currentLeftMouseDownNow == false && previousLeftMouseDown == true;
+
+        if (justClicked && inRange == true && interacting == false && Timer.isPaused() == false)
+        {
             // adjust?
             Vector3 CameraPos = Camera.main.transform.position;
-            worldMousePosition += new Vector2(CameraPos.x, CameraPos.y);
-
-            //GameObject.Find("OrbPosition").transform.position = worldMousePosition;
-            
-            // pathfind
-            var start = player.transform.position;
-            var clickedRoom = world.GetRoomAt(worldMousePosition);
-            if (player.CurrentRoom == null)
-            {
-                previousLeftMouseDown = currentLeftMouseDown;
-                return;
-            }
-            if (clickedRoom == null || clickedRoom == player.CurrentRoom)
-            {
-                // If no room is clicked, just move in the current room.
-                var currentRoom = player.CurrentRoom;
-                var destination = currentRoom.ClampGlobal(worldMousePosition);
-                var path = currentRoom.GetInteriorPathFrom(start, destination);
-                player.MoveAlongPath(path);
-            }
-            else
-            {
-                // pathfind to other room
-                var destination = clickedRoom.ClampGlobal(worldMousePosition);
-                var path = world.GetExteriorPathFrom(start, destination);
-                player.MoveAlongPath(path);
-            }
-            
+            var dest = new Vector2(worldMousePosition.x + CameraPos.x, worldMousePosition.y + CameraPos.y);
+            DoClickAndMoveTo(dest);
         }
-        previousLeftMouseDown = currentLeftMouseDown;
+        previousLeftMouseDown = currentLeftMouseDownNow;
+
+        // use opportunity to try and move
+        // for every FREQUENCY
+        if (IsMouseButtonHeld &&
+            (consecutiveHeldFrames - FRAMES_REQUIRED_FOR_HOLD_CLICK) % FRAMES_FOR_AUTO_MOVE_FREQUENCY == 0)
+        {
+            //auto move
+            Debug.Log("AutoClick!");
+            Vector3 CameraPos = Camera.main.transform.position;
+            var dest = new Vector2(worldMousePosition.x + CameraPos.x, worldMousePosition.y + CameraPos.y);
+            DoClickAndMoveTo(dest);
+        }
+    }
+
+    private void DoClickAndMoveTo(Vector2 where)
+    {
+        //GameObject.Find("OrbPosition").transform.position = where;
+
+        // pathfind
+        var start = player.transform.position;
+        var clickedRoom = world.GetRoomAt(where);
+        if (player.CurrentRoom == null)
+        {
+            return;
+        }
+        if (clickedRoom == null || clickedRoom == player.CurrentRoom)
+        {
+            // If no room is clicked, just move in the current room.
+            var currentRoom = player.CurrentRoom;
+            var destination = currentRoom.ClampGlobal(where);
+            var path = currentRoom.GetInteriorPathFrom(start, destination);
+            player.MoveAlongPath(path);
+        }
+        else
+        {
+            // pathfind to other room
+            var destination = clickedRoom.ClampGlobal(where);
+            var path = world.GetExteriorPathFrom(start, destination);
+            player.MoveAlongPath(path);
+        }
+
     }
 }
